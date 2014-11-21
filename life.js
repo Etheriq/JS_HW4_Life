7195cbef36db22ea17af48f18632f9101623844d
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2014-11-21 13:55:32 +0200

    fix Life game

--- life.js
+++ life.js
@@ -1,11 +1,15 @@
-function lifeCicle(map){
+function lifeCicle(mapOriginal){
     var i = 0,
         j = 0,
-        workOut = [],
-        mapLen = map.length;
+        map = [];
 
-    map.forEach(function(val, ind){
-        var work = [];
+    mapOriginal.forEach(function(subArray) {
+        map.push(subArray.concat());
+    });
+
+    var mapLen = map.length;
+
+    mapOriginal.forEach(function(val, ind){
         val.forEach(function(valI, indI, arr){
             var z = ind;
             var cur = valI;
@@ -15,24 +19,24 @@ function lifeCicle(map){
 
             if (z > 0){
                 if (indI - 1 >= 0) {
-                    tmpArr.push(map[z - 1][indI - 1]);
+                    tmpArr.push(mapOriginal[z - 1][indI - 1]);
                 }
-                tmpArr.push(map[z - 1][indI]);
+                tmpArr.push(mapOriginal[z - 1][indI]);
                 if ( indI + 1 <= arr.length) {
-                    tmpArr.push(map[z - 1][indI + 1]);
+                    tmpArr.push(mapOriginal[z - 1][indI + 1]);
                 }
             }
 
-            indI - 1 >= 0 ? tmpArr.push(map[z][indI - 1]) : '';
-            indI + 1 <= arr.length + 1 ? tmpArr.push(map[z][indI + 1]): '';
+            indI - 1 >= 0 ? tmpArr.push(mapOriginal[z][indI - 1]) : '';
+            indI + 1 <= arr.length + 1 ? tmpArr.push(mapOriginal[z][indI + 1]): '';
 
             if (z + 1 < mapLen) {
                 if (indI - 1 >= 0) {
-                    tmpArr.push(map[z + 1][indI - 1]);
+                    tmpArr.push(mapOriginal[z + 1][indI - 1]);
                 }
-                tmpArr.push(map[z + 1][indI]);
+                tmpArr.push(mapOriginal[z + 1][indI]);
                 if ( indI + 1 <= arr.length) {
-                    tmpArr.push(map[z + 1][indI + 1]);
+                    tmpArr.push(mapOriginal[z + 1][indI + 1]);
                 }
             }
             tmpArr.forEach(function(el){
@@ -48,10 +52,8 @@ function lifeCicle(map){
             if (cur == false && live == 3) {
                 map[ind][indI] = true;
             }
-            work.push(map[ind][indI]);
         });
-        workOut.push(work);
     });
 
-    return workOut;
+    return map;
 }
